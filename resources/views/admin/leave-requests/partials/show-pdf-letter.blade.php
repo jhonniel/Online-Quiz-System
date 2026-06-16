dbaa8ccddde41d133819a8e237edb5053bc12ce0
jRy <jhanniel@jRy-2.local>
2026-06-16 17:34:46 +0800

    leave gender and pdf leave

--- resources/views/admin/leave-requests/partials/show-pdf-letter.blade.php
+++ resources/views/admin/leave-requests/partials/show-pdf-letter.blade.php
@@ -26,9 +26,8 @@
             <p>
                 If my leave of absence is approved, I'll try my best to assist with any questions by phone call
                 or chat provided that I have the means or I can connect with the internet.
+                <strong>Additional info:</strong>  {!! nl2br(e(trim((string) ($leaveRequest->reason ?? '')))) !!}
             </p>
-            <p style="white-space: pre-line;"><strong>Additional info:</strong>
-{{ $leaveRequest->reason }}</p>
             <p>
                 Please let me know if you have any questions and an appropriate time for us to speak to
                 discuss the terms of my leave of absence.
